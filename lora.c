6eaa47526c28c55df8aeb5730cafc049e884d47c
dphilhow <dphilhow@purdue.edu>
2025-11-10 13:15:40 -0500

    header files for stm32 and LoRa module

--- lora.c
+++ lora.c
@@ -1,18 +1,14 @@
-//need a single receive mode or way to know if some data was received in continuous mode even if it was not a complete message
-//RYLR998 has SMART RECEIVING POWER SAVING MODE, which may work, but also may not (datasheet is very bad)
-    //probably won't be able to tell if it received part of a message
+#include "stm32f0xx.h"
+//lora functions
 
-//need to find a new LoRa module?
-    //one in lab does not have single receive or any way to know 
-    //GAMMA62TR-89D no
+//initalize/setup
+//setup UART
 
-//DS_SX1276-7-8-9_W_APP_V7.
-    //has single receive mode, listens for a valid premable, so could just make first message's data a bunch of preambles 
-    //then send the data next time 
-    //is expensive and is just the chip (no PCB with antenna)
+//change mode to send
 
-//https://www.digikey.com/en/products/detail/seeed-technology-co-ltd/113990830/16652881
-    //uses DS_SX1276-7-8-9
-    //https://files.seeedstudio.com/products/113990830/document/Datasheet_LoRaST(1).pdf
-    //should be easy to program
-    //$31, not many in stock
+//change mode to receive
+
+//create data packet
+
+//https://mm.digikey.com/Volume0/opasdata/d220001/medias/docus/2527/113060006_Web.pdf
+//https://semtech.my.salesforce.com/sfc/p/#E0000000JelG/a/2R0000001Rbr/6EfVZUorrpoKFfvaF_Fkpgp5kzjiNyiAbqcpqh9qSjE 
