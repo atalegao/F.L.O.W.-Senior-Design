271d56137670f44432aa30b40c3906e0a802f452
dphilhow <dphilhow@purdue.edu>
2026-01-30 12:07:32 -0500

    lora now has no errors, still need to optimize using HAL functions

--- lora/Core/Inc/main.h
+++ lora/Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LoRa_power_Pin GPIO_PIN_8
+#define LoRa_power_GPIO_Port GPIOC
 
 /* USER CODE BEGIN Private defines */
 
