60884a79e2659f9e2de23378a39284b13cbd0a69
vanderg0 <142921863+vanderg0@users.noreply.github.com>
2026-04-02 16:46:05 -0400

    Ultrasonic code!

--- main.c
+++ main.c
@@ -21,7 +21,6 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -44,35 +43,47 @@ UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-uint8_t a7670_byte;
-uint8_t pc_byte;
+typedef enum {
+	STATE_IDLE,
+	STATE_SAMPLING,
+	STATE_REPORTING
+} SystemState_t;
 
-#define FLOOD_THRESHOLD_MM 3000
+SystemState_t current_state = STATE_IDLE;
+uint32_t state_timer = 0;
+uint32_t trigger_timer = 0;
+
+uint8_t rx_data;
+uint8_t packet[4];
+uint8_t packet_idx = 0;
+
+#define FILTER_SIZE 20
+uint32_t readings[FILTER_SIZE] = {0};
+uint8_t read_idx = 0;
+uint32_t distance_mm = 0;
+
+uint64_t long_term_sum = 0;
+uint32_t sample_count = 0;
+
+volatile uint8_t data_ready = 0;
+
+const uint32_t BREAK_TIME = 1 * 60 * 1000; //ten minutes idle
+const uint32_t ON_TIME = 1 * 60 * 1000; //one minute sampling
+const uint32_t TRIGGER_RATE = 200; //polling freq, 5 times per sec during polling
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_USART2_UART_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
-
+void trigger_sensor_reading(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-/*void uart2_send(const char *msg)
-{
-    HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-}*/
-//void uart2_send(char *msg)
-//{
-//    HAL_UART_Transmit(&huart2,
-//                      (uint8_t *)msg,
-//                      strlen(msg),
-//                      HAL_MAX_DELAY);
-//}
 /* USER CODE END 0 */
 
 /**
@@ -104,31 +115,78 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_USART2_UART_Init();
   MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-  HAL_UART_Receive_IT(&huart1, &a7670_byte, 1);
-  HAL_UART_Receive_IT(&huart2, &pc_byte, 1);
-  char *msg = "\r\n--- STM32 LTE Bridge Ready ---\r\n";
-  HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), 100);
+  HAL_UART_Receive_IT(&huart1, &rx_data, 1);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  uint32_t last_poll_time = 0;
+  uint32_t poll_interval = 1000; //# ms between readings
   while (1)
   {
-//	  // 1. Check if PC sent something (USART2) -> Forward to LTE (USART1)
-//	      if (HAL_UART_Receive(&huart2, &rx_byte, 1, 10) == HAL_OK)
-//	      {
-//	          HAL_UART_Transmit(&huart1, &rx_byte, 1, 10);
-//	      }
-//
-//	      // 2. Check if LTE sent something (USART1) -> Forward to PC (USART2)
-//	      if (HAL_UART_Receive(&huart1, &rx_byte, 1, 10) == HAL_OK)
-//	      {
-//	          HAL_UART_Transmit(&huart2, &rx_byte, 1, 10);
-//	      }
+	  uint32_t now = HAL_GetTick();
 
+	  switch (current_state) {
+	  	  case STATE_IDLE:
+	  		  if (now - state_timer >= BREAK_TIME) { //ten minutes
+	  				  state_timer = now;
+	  		  	  	  long_term_sum = 0;
+	  		  	  	  sample_count = 0;
+	  		  	  	  current_state = STATE_SAMPLING;
+	  		  	  	  printf("Starting Sample \r\n");
+	  		  }
+	  		  break;
+
+	  	  case STATE_SAMPLING:
+	  		  //trigger reading at specific rate
+	  		  if (now - trigger_timer >= TRIGGER_RATE) {
+	  			  trigger_sensor_reading();
+	  			  trigger_timer = now;
+	  		  }
+	  		  //THIS IS OPTIONAL REAL TIME PRINTING DURING MINUTE POLL
+	  		  if (data_ready) {
+	  			  printf("current distance reading: %lu mm\r\n", distance_mm);
+	  			  data_ready = 0;
+	  		  }
+
+	  		  // end sampling window
+	  		  if (now - state_timer >= ON_TIME) {
+	  			  current_state = STATE_REPORTING;
+	  		  }
+	  		  break;
+
+	  	  case STATE_REPORTING:
+	  		  if (sample_count > 0) {
+	  			  uint32_t final_report = (uint32_t)(long_term_sum / sample_count);
+	  			  printf("average reading from poll: %lu mm (from %lu samples)\r\n", final_report, sample_count);
+	  		  }
+	  		  state_timer = now;
+	  		  current_state = STATE_IDLE;
+	  		  printf("entering idle\r\n");
+	  		  break;
+	  }
+
+//	  if (HAL_GetTick() - last_poll_time >= poll_interval) {
+//		  trigger_sensor_reading();
+//
+//		  //wait for interrupt to finish packet (100 ms timeout)
+//		  uint32_t start_wait = HAL_GetTick();
+//		  while(!data_ready && (HAL_GetTick() - start_wait < 100));
+//
+//		  if(data_ready) {
+//			  printf("Distance: %lu mm\r\n", distance_mm);
+//		  } else {
+//			  printf("sensor timeout \r\n");
+//		  }
+//		  last_poll_time = HAL_GetTick();
+//	  }
+//
+//
+//	  HAL_Delay(500);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -153,9 +211,10 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_5;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -166,7 +225,7 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
@@ -200,7 +259,7 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
+  huart1.Init.BaudRate = 9600;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
@@ -235,7 +294,7 @@ static void MX_USART2_UART_Init(void)
 
   /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
+  huart2.Init.BaudRate = 9600;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
@@ -261,6 +320,7 @@ static void MX_USART2_UART_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
   /* USER CODE BEGIN MX_GPIO_Init_1 */
 
   /* USER CODE END MX_GPIO_Init_1 */
@@ -268,34 +328,76 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PA0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
   /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-	if (huart->Instance == USART2) {
-	        HAL_UART_Receive_IT(&huart2, &pc_byte, 1);
-	        HAL_UART_Transmit(&huart1, &pc_byte, 1, 10);
-	    }
+void trigger_sensor_reading(void) {
+	//reset state for new packet
+	packet_idx = 0;
+	data_ready = 0;
 
-	    if (huart->Instance == USART1) {
-	        HAL_UART_Receive_IT(&huart1, &a7670_byte, 1);
-	        HAL_UART_Transmit(&huart2, &a7670_byte, 1, 10);
-	    }
+	//pull RX low to trigger
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+	HAL_Delay(1);
+	//set back to high aka idle
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
 }
+//link printf to serial port
+int __io_putchar(int ch) {
+	HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+	return ch;
+}
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+    if (huart->Instance == USART1) {
+        // State Machine to align packet
+        if (packet_idx == 0 && rx_data != 0xFF) {
+            // wait for header byte
+        	packet_idx = 0;
+        } else {
+        	packet[packet_idx++] = rx_data;
+        }
 
-void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
-    if (huart->ErrorCode & HAL_UART_ERROR_ORE) {
-        // Read the data register to clear the ORE flag
-        __HAL_UART_CLEAR_OREFLAG(huart);
-        // Restart the interrupt
-        if(huart->Instance == USART1) HAL_UART_Receive_IT(&huart1, &a7670_byte, 1);
-        if(huart->Instance == USART2) HAL_UART_Receive_IT(&huart2, &pc_byte, 1);
+        //once 4 bytes
+        if (packet_idx == 4) {
+            // Sum the first three bytes and mask to 8 bits
+            uint8_t sum = (packet[0] + packet[1] + packet[2]) & 0xFF;
+
+            if (sum == packet[3]) {
+                uint8_t raw_val = (packet[1] << 8) | packet[2];
+                readings[read_idx] = raw_val;
+                read_idx = (read_idx + 1) % FILTER_SIZE;
+                uint32_t total = 0;
+                for(int i = 0; i < FILTER_SIZE; i++) {
+                	total += readings[i];
+                }
+                distance_mm = total / FILTER_SIZE;
+
+                if (current_state == STATE_SAMPLING) {
+                	long_term_sum += raw_val;
+                	sample_count++;
+                }
+                data_ready = 1; //signal finish valid reading
+            }
+            packet_idx = 0; // reset index
+        }
+
+        HAL_UART_Receive_IT(&huart1, &rx_data, 1);
     }
 }
+
 /* USER CODE END 4 */
 
 /**
